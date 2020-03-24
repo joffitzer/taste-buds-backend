e77ba36bf665fe7d598be7afd1b9d995502dad53
joffitzer <joffitzer@gmail.com>
2020-03-24 09:23:56 -0400

    like feature posting to backend

--- app/controllers/api/v1/likes_controller.rb
+++ app/controllers/api/v1/likes_controller.rb
@@ -3,4 +3,16 @@ class Api::V1::LikesController < ApplicationController
         likes = Like.all
         render json: LikeSerializer.new(likes)
     end 
+
+    def create 
+        @likes = Like.create(like_params)
+        render json: @like, except: [:updated_at]
+    end 
+
+end
+
+private
+
+def like_params
+    params.permit(:user_id, :restaurant_id)
 end
--- app/serializers/like_serializer.rb
+++ app/serializers/like_serializer.rb
@@ -1,6 +1,15 @@
 class LikeSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :created_at, :user, :restaurant 
+  attributes :created_at 
+  attribute :restaurant do |like|
+    {restaurant: like.restaurant}
+  end 
+  attribute :user do |like|
+    {user: like.user}
+  end 
+  # attribute :job do |user_app|
+  #   {job: user_app.job, employer: user_app.job.employer}
+  # end 
 end
 
 
